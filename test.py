09d3e6840c9e6c65b692e5fa145f233208f4b583
Alberto Pérez Caro <acaro@autotechconnect.com>
2024-07-18 13:27:14 +0200

    estrategia alberto

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from telegram.ext import Updater, CallbackContext, CommandHandler, JobQueue
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import csv
 import os
+from datetime import datetime, timedelta
 import yfinance as yf
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -22,6 +23,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # Nombre del archivo
 filename_1m = 'ordenes_TSLA.csv'
 filename_5m = 'ordenes_NVDA.csv'
+filename_Alberto = 'ordenes_Alberto.csv'
 
 
 #OFICIAL: 7294424253:AAG6rjghmNpRsyMYTQWjogqEiRJDDjflloM
@@ -89,6 +91,13 @@ latest_data_1h = {}
 # Cartera con la que opero
 CarteraGold_1h = 0
 CarteraUSDT_1h = 1000
+
+precio_apertura=0
+momento_compra_alberto=True
+momento_compra_max=True
+precioMax_Alberto=0
+precioMin_Alberto=0
+habilitaEstrategiaAlberto=False
 
 # Función para crear el archivo si no existe
 def create_csv_if_not_exists(filename):
@@ -278,10 +287,99 @@ def get_price_and_send_5m(context: CallbackContext) -> None:
         pass
 
 
+def comprobar_hora():
+    # Obtener la hora actual
+    hora_actual = datetime.now().hour
+    ret=False
+    # Comprobar si es las 2 PM o las 3 PM
+    if hora_actual == 14:
+        #print("Son las 2 de la tarde.")
+        ret=True
+    elif hora_actual == 15:
+        #print("Son las 3 de la tarde.")
+        ret = True
+    return ret
+
+def obtener_precio_apertura(ticker):
+    global precio_apertura
+    # Obtener el símbolo del ticker
+    stock = yf.Ticker(ticker)
+
+    # Obtener la fecha de hoy y la fecha de ayer
+    hoy = datetime.now()
+    ayer = hoy - timedelta(1)
+
+    # Descargar datos históricos del día de ayer y de hoy
+    datos = stock.history(start=ayer, end=hoy)
+
+    # Obtener el precio de apertura de hoy
+    precio_apertura = datos['Open'].iloc[-1]
+
+    return precio_apertura
+
+def estrategia_alberto(context: CallbackContext) -> None:
+    global price_5m,precio_apertura,price_5m,momento_compra_alberto,momento_compra_max,precioMax_Alberto,precioMin_Alberto,habilitaEstrategiaAlberto
+    ticker = "NVDA"
+    if comprobar_hora() or precio_apertura==0:
+        obtener_precio_apertura(ticker)
+
+    precioMax_Alberto=precio_apertura*1.005
+    precioMin_Alberto=precio_apertura*0.995
+    if habilitaEstrategiaAlberto:
+        if price_5m==0:
+            signal_message = f"el precio es 0"
+            context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+        else:
+            if momento_compra_alberto and price_5m!=0:
+                if precioMax_Alberto<price_5m:
+                    signal_message = f"(NVDA ESTRATEGIA ALBERTO)\nOperacion de venta en {price_5m}\nprecioMax_Alberto:{precioMax_Alberto}\nApertura hoy:precio_apertura{precio_apertura} "
+                    add_order("ORDEN_BAJA", str(price_5m), filename_Alberto)
+                    context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+                    #print("haz una operacion de venta")
+                    momento_compra_max=True
+                    momento_compra_alberto=False
+
+                if precioMin_Alberto>price_5m:
+                    signal_message = f"(NVDA ESTRATEGIA ALBERTO)\nOperacion de compra en {price_5m}\nprecioMin_Alberto:{precioMin_Alberto}\nApertura hoy:precio_apertura{precio_apertura}"
+                    add_order("ORDEN_SUBE", str(price_5m), filename_Alberto)
+                    context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+                    #print("haz una operacion de compra")
+                    momento_compra_max = False
+                    momento_compra_alberto=False
+            else:
+                if momento_compra_max:
+                    if precioMin_Alberto>price_5m:
+                        signal_message = f"(NVDA ESTRATEGIA ALBERTO)\nCIERRA Operacion de venta en {price_5m}\nprecioMin_Alberto:{precioMin_Alberto}\nApertura hoy:precio_apertura{precio_apertura}"
+                        add_order("CIERRA_ORDEN_BAJA", str(price_5m), filename_Alberto)
+                        context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+                        #print("cierra la operacion de venta")
+                        momento_compra_alberto = True
+                else:
+                    if precioMax_Alberto < price_5m:
+                        signal_message = f"(NVDA ESTRATEGIA ALBERTO)\nCIERRA\n Operacion de compra en {price_5m}\nprecioMax_Alberto:{precioMax_Alberto}\nApertura hoy:precio_apertura{precio_apertura}"
+                        add_order("CIERRA_ORDEN_SUBE", str(price_5m), filename_Alberto)
+                        context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+                        #print("cierra la operacion de compra")
+                        momento_compra_alberto = True
+
+
+
 # Función que envía un mensaje cada 10 minutos para indicar que el bot sigue vivo
 def send_alive_message(context: CallbackContext) -> None:
     global price
     signal_message = f"Precio: {price} USDT"
+    context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+
+def send_habilitaAlberto(update: Update, context: CallbackContext) -> None:
+    global habilitaEstrategiaAlberto
+    habilitaEstrategiaAlberto = True
+    signal_message = f"Estrategia Alberto habilitada!"
+    context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+
+def send_NohabilitaAlberto(update: Update, context: CallbackContext) -> None:
+    global habilitaEstrategiaAlberto
+    habilitaEstrategiaAlberto = False
+    signal_message = f"Estrategia Alberto deshabilitada!"
     context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
 
 def send_noOperar_1m(update: Update, context: CallbackContext) -> None:
@@ -308,6 +406,8 @@ def send_comandos(update: Update, context: CallbackContext) -> None:
         f"/operarTSLA\n"
         f"/noOperarNVDA\n"
         f"/operarNVDA\n"
+        f"/send_habilitaAlberto\n"
+        f"/send_NohabilitaAlberto\n"
         f"/comandos\n"
     )
     context.bot.send_message(chat_id=CHAT_ID, text=summary_message)
@@ -334,6 +434,11 @@ def send_venta_5m(update: Update, context: CallbackContext) -> None:
     global ventaObligada_5m
     ventaObligada_5m = True
     signal_message = f"venta obligada recibida! (5m)"
+    context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
+
+def send_resume_Alberto(update: Update, context: CallbackContext) -> None:
+    global precioMax_Alberto,precioMin_Alberto,precio_apertura,price_5m
+    signal_message = f"(ESTRATEGIA ALBERTO NVDA)\nprecio_apertura:{precio_apertura}\nsoporte Superior: {precioMax_Alberto}\nsoporte Inferior:{precioMin_Alberto}\nprice_5m:{price_5m}"
     context.bot.send_message(chat_id=CHAT_ID, text=signal_message)
 
 
@@ -421,6 +526,7 @@ def main() -> None:
     # Crear el archivo si no existe
     create_csv_if_not_exists(filename_1m)
     create_csv_if_not_exists(filename_5m)
+    create_csv_if_not_exists(filename_Alberto)
     while True:
         try:
             # Crear el updater y pasarlo a tu bot token
@@ -435,6 +541,8 @@ def main() -> None:
             # Agregar un trabajo recurrente que se ejecuta cada minuto para obtener el precio y enviarlo
             job_queue.run_repeating(get_price_and_send_1m, interval=60, first=0)
             job_queue.run_repeating(get_price_and_send_5m, interval=60, first=0)
+            job_queue.run_repeating(estrategia_alberto, interval=60, first=0)
+
 
             # Agregar un trabajo recurrente que se ejecuta cada 10 minutos para enviar un mensaje "Sigo vivo"
             job_queue.run_repeating(send_alive_message, interval=10800, first=0)
@@ -442,6 +550,7 @@ def main() -> None:
             # Añadir manejador de comando para /resumen
             updater.dispatcher.add_handler(CommandHandler('resumenTSLA', send_summary_1m))
             updater.dispatcher.add_handler(CommandHandler('resumenNVDA', send_summary_5m))
+            updater.dispatcher.add_handler(CommandHandler('resumenAlberto', send_resume_Alberto))
             updater.dispatcher.add_handler(CommandHandler('ordenesTSLA', send_NumOrd_message_1m))
             updater.dispatcher.add_handler(CommandHandler('ordenesNVDA', send_NumOrd_message_5m))
             updater.dispatcher.add_handler(CommandHandler('ventaTSLA', send_venta_1m))
@@ -450,7 +559,11 @@ def main() -> None:
             updater.dispatcher.add_handler(CommandHandler('noOperarNVDA', send_noOperar_5m))
             updater.dispatcher.add_handler(CommandHandler('operarTSLA', send_Operar_1m))
             updater.dispatcher.add_handler(CommandHandler('operarNVDA', send_Operar_5m))
+            updater.dispatcher.add_handler(CommandHandler('send_habilitaAlberto', send_habilitaAlberto))
+            updater.dispatcher.add_handler(CommandHandler('send_NohabilitaAlberto', send_NohabilitaAlberto))
+
             updater.dispatcher.add_handler(CommandHandler('comandos', send_comandos))
+
 
             # Empezar el bot
             updater.start_polling()
